4ae83f015d83ce060eec064ec46fb93e3d1860a3
H-Menendez <64504589+H-Menendez@users.noreply.github.com>
2020-05-07 15:18:36 +0200

    Paragraphe
    Ajout des paragraphes 301 et 208.

--- principal.py
+++ principal.py
@@ -1,38 +1,35 @@
-# Squelette de script pygame
-import pygame
-import random
+def paragraphe_301():
+    texte=["A votre gauche, du côté ouest du passage, il y a une porte de bois brut",
+           "grossièrement taillé. Vous tendez l'oreille et vous percevez un bruit qui",
+           "pourrait être le ronflement d'une quelconque créature."]
+    
+    for loop in range(len(texte)):
+        print(texte[loop])
 
-WIDTH = 360 # taille écran largeur hauteur
-HEIGHT = 480
-FPS = 30 # images par seconde
+    print("Désirez-vous ouvrir cette porte ou continuer vers le nord?")
+    
+    choix=input("Entrez votre choix porte ou nord :")
 
-# couleurs prédéfinies
-BLACK = (0, 0, 0)
-ROUGE = (255,0,0)
+    if choix=="porte":
+        paragraphe_82()
+    elif choix=="nord":
+        paragraphe_208()
+        
 
-# initialize pygame and create window
-pygame.init()
-pygame.mixer.init() # initialisation des sons
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Sorcery")
-clock = pygame.time.Clock()
+def paragraphe_208():
+    texte=["Un peu plus loin dans le passage qui longe le mur ouest, vous trouvez",
+           "une autre porte. Vous collez votre oreille contre le panneau, mais vous",
+           "n'entendez rien."]
+    
+    for loop in range(len(texte)):
+        print(texte[loop])
 
-# boucle de jeu
-running = True
-while running:
-    # assure que la boucle fonctionne au rythme donné par FPS
-    clock.tick(FPS)
-    # traitement des événements
-    for event in pygame.event.get():
-        # evenement fermeture de la fenetre pygame
-        if event.type == pygame.QUIT:
-            running = False
+    print("Désirez-vous ouvrir cette porte ou poursuivre votre chemin?")
+    
+    choix=input("Entrez votre choix porte ou poursuivre :")
 
-    # mise à jour des variables
+    if choix=="porte":
+        paragraphe_397()
+    elif choix=="poursuivre":
+        paragraphe_363()
 
-    # dessin
-    screen.fill(BLACK)
-    # affichage
-    pygame.display.flip()
-
-pygame.quit()
